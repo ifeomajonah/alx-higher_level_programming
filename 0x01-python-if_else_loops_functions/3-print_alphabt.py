bf550af81b2a220939ff2d9038d791ed497f7d54
Ifeoma Jonah <adejokejonah@gmail.com>
2023-11-01 15:32:12 -0700

    free

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end="")
+for character in range(97, 123):
+    if (character == 101 or character == 113):
+        continue
+    print("{:c}".format(character), end='')
